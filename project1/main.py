95c42f34c5a73e7a1eb1282f3181f6960ec1c425
Fredrik <fredrvaa@stud.ntnu.no>
2022-02-02 12:04:59 +0100

    Set good parameters to display entropy and fitness

--- project1/main.py
+++ project1/main.py
@@ -1,25 +1,54 @@
 import numpy as np
 
-from evolution.genetic_algorithm import SimpleGeneticAlgorithm, DeterministicCrowding, ProbabilisticCrowding
-from fitness.LinReg import LinReg
+from evolution.genetic_algorithm import SimpleGeneticAlgorithm, DeterministicCrowding, ProbabilisticCrowding, \
+    GeneralizedCrowding
+from evolution.visualizer import Visualizer
 from fitness.fitness_functions import SineFitness, SquaredFitness, LinearWithSineFitness
 from data_utils.dataset import Dataset
 
 dataset = Dataset.read_file('dataset.txt')
-#model = LinReg()
 
 fitness_function = SineFitness(interval=(0, 128), target_interval=None, distance_factor=0.1)
+termination_fitness = None
+algos = []
 
-ga = SimpleGeneticAlgorithm(
-    population_size=200,
+algos.append(SimpleGeneticAlgorithm(
+    population_size=400,
     n_bits=15,
     fitness_function=fitness_function,
-    p_cross_over=0.6,
+    p_cross_over=0.7,
     p_mutation=0.001
-)
+))
 
-ga.fit(generations=1000, verbose=True, visualize=False)
-ga.visualize_fitness()
-ga.visualize_entropy()
+algos.append(GeneralizedCrowding(
+    population_size=400,
+    n_bits=15,
+    fitness_function=fitness_function,
+    p_cross_over=0.7,
+    p_mutation=0.001,
+    scaling_factor=0.5,
+))
 
-#print(model.get_fitness(dataset.x, dataset.y))
+algos.append(DeterministicCrowding(
+    population_size=400,
+    n_bits=15,
+    fitness_function=fitness_function,
+    p_cross_over=0.7,
+    p_mutation=0.001,
+))
+
+algos.append(ProbabilisticCrowding(
+    population_size=400,
+    n_bits=15,
+    fitness_function=fitness_function,
+    p_cross_over=0.7,
+    p_mutation=0.001,
+))
+
+for algo in algos:
+    algo.fit(generations=100, verbose=False, visualize=False, vis_sleep=0, termination_fitness=termination_fitness)
+
+visualizer = Visualizer(algos)
+
+visualizer.visualize_fitness()
+visualizer.visualize_entropy()
