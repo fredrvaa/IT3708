5b9c0f984feb24b1debf209230a58d30c8b48054
Fredrik <fredrvaa@stud.ntnu.no>
2022-02-03 10:40:55 +0100

    Change from class method to static method in dataset class

--- project1/data_utils/dataset.py
+++ project1/data_utils/dataset.py
@@ -1,22 +1,22 @@
-import os
 from dataclasses import dataclass
 
 import numpy as np
 import pandas as pd
 
 
-
 @dataclass
 class Dataset:
+    """Dataclass for loading and storing a dataset."""
     x: np.ndarray
     y: np.ndarray
 
-    @classmethod
-    def read_file(cls, file_name: str) -> 'Dataset':
+    @staticmethod
+    def load(file_name: str) -> 'Dataset':
+        """Loads dataset from file
+
+        :param file_name: Path to file from current directory.
+        :return: A Dataset object.
+        """
         data = pd.read_csv(file_name, sep=',', header=None)
         x, y = data.iloc[:, :-1], data.iloc[:, -1]
         return Dataset(x.to_numpy(), y.to_numpy())
-
-if __name__ == '__main__':
-    d = Dataset.read_file('dataset.txt')
-    print(d.x.shape, d.y.shape)
